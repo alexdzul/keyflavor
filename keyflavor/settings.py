cdf3414a385fb1f88c477f48ad0c6d49e4a170a1
Alex Dzul <alexexc2@gmail.com>
2015-04-03 15:11:59 -0600

    Modificación del settings para que podamos trabajar con PostgreSQL en lugar de SQLite3

--- keyflavor/settings.py
+++ keyflavor/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'keyflavor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'keyflavor_db',
+        'USER': 'admin',
+        'PASS': 'admin',
     }
 }
 
